0dc5756d443e6e6358a6ab0eb4e64a8feb0c0101
Yash708262 <yash708262@gmail.com>
2025-11-09 11:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+
+    // Allow only digits
+    if (/[^0-9]/.test(value)) {
+      e.target.value = "";
+      return;
+    }
+
+    // Move to next input if filled
+    if (value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      // Clear the field first
+      if (input.value === "" && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = "";
+      } else {
+        input.value = "";
+      }
+    }
+  });
+});
